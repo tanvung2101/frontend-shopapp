e96b0d7a11e066f4ea7d05991fb4cefa7edc626d
TanVung <lientanvung2101@gmail.com>
2025-05-02 16:44:16 +0700

    update authApi

--- src/apis/authApis.tsx
+++ src/apis/authApis.tsx
@@ -3,27 +3,32 @@ import axiosInstance from "./axios";
 
 export type UpdateUser = Omit<Profile, "id" | "role"| "email">;
 
+const requestConfig = {
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+};
+
 const accountApis = {
   login(payload: LoginData): Promise<LoginResponse> {
-    return axiosInstance.post("users/login", payload);
+    return axiosInstance.post("users/login", payload,requestConfig);
   },
   register(payload: RegisterData): Promise<RegisterResponse> {
-    return axiosInstance.post("users/register", payload);
+    return axiosInstance.post("users/register", payload, requestConfig);
   },
   forgot_password(payload: { email: string }): Promise<{ email: string }> {
-    return axiosInstance.post("users/forgot-password", payload);
+    return axiosInstance.post("users/forgot-password", payload, requestConfig);
   },
   reset_password(payload: {
     forgot_password_token: string;
     password: string;
   }): Promise<RegisterResponse> {
-    return axiosInstance.post("users/rest-password", payload);
+    return axiosInstance.post("users/rest-password", payload, requestConfig);
   },
   updateUser(payload:UpdateUser, userId: number): Promise<{message: string, data: Profile}> {
-    return axiosInstance.put(`users/${userId}`, payload,);
+    return axiosInstance.put(`users/${userId}`, payload,requestConfig);
   },
   logout():Promise<{message: string}>{
-    return axiosInstance.get('users/logout')
+    return axiosInstance.get('users/logout', requestConfig)
   }
 };
 
